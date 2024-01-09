5c01d0a0f8eb58192562c937749bf684518e7e18
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2024-01-10 02:40:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+// Microtasks
+console.log('Microtask 1'); 
+
+setTimeout(() => {
+  // Macrotask 1
+  console.log('Macrotask 1');
+  
+  setTimeout(() => {
+    // Macrotask 2
+    console.log('Macrotask 2');
+  }, 0);
+  
+  Promise.resolve()
+    .then(() => {
+      // Microtask 2
+      console.log('Microtask 2'); 
+    });
+}, 0);
+
+Promise.resolve()
+  .then(() => {
+    // Microtask 3
+    console.log('Microtask 3');
+  });
+  
+// Microtask 4  
+console.log('Microtask 4');
